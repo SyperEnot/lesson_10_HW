7c477bbb1b1baa2d092fa8de69c90350fea66074
Stas Yorohov <stas_yorohov@mail.ru>
2024-10-08 22:48:46 +0300

    add homework lesson 10

---
+++ src/test/java/StepsTest.java
@@ -0,0 +1,42 @@
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest {
+
+    @Test
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchForRepository(String repo) {
+        $(".search-input").click();
+        $("#query-builder-test").sendKeys(repo);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Кликаем по ссылке репозитория {repo}")
+    public void clickOnRepoLink(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Step("Открываем таб Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issue с номером {issue}")
+    public void shouldSeeIssueWithText(String issue) {
+        $(withText(issue)).should(Condition.exist);
+    }
+
+}
+
